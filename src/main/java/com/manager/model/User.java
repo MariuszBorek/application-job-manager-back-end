a72b841ac5ff875b79a96db33ef200faa6c65f71
Mariusz Borek <mariuszborek242@gmail.com>
2020-10-30 19:49:09 +0100

    added User, Project entities

---
+++ src/main/java/com/manager/model/User.java
@@ -0,0 +1,97 @@
+package com.manager.model;
+
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.Objects;
+
+@NoArgsConstructor
+public class User {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Map<Integer, Project> projects;
+
+    public User(Integer id,
+                String name,
+                String surname,
+                String email,
+                String password,
+                Map<Integer,
+                        Project> projects) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.projects = projects;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Map<Integer, Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Map<Integer, Project> projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, email, password);
+    }
+}
