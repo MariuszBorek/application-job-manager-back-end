43e7ec308501b97e0d2a6f18d991d7af277d8938
Mariusz Borek <mariuszborek242@gmail.com>
2020-11-13 00:50:24 +0100

    created project and task service for user

--- src/main/java/com/manager/controller/UserController.java
+++ src/main/java/com/manager/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.manager.controller;
 
+import com.manager.model.Project;
 import com.manager.model.Sheet;
+import com.manager.model.Task;
 import com.manager.model.User;
 import com.manager.service.UserService;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +34,44 @@ public class UserController {
     @PostMapping
     public User addUser(@RequestBody final User user) {
         return userService.addUser(user);
+    }
+
+    @PostMapping("/{id}")
+    public User addUser(@PathVariable("id") final String id,
+                        @RequestBody final Project project) {
+        return userService.addProject(id, project);
+    }
+
+    @PostMapping("/project/task/{userId}/{projectId}")
+    public Task addTaskToProject(@PathVariable("userId") final String userId,
+                                 @PathVariable("projectId") final String projectId,
+                                 @RequestBody final Task task) {
+        return userService.addTask(userId, projectId, task);
+    }
+
+    @GetMapping("/projects/{id}")
+    public List<Project> getUser(@PathVariable("id") final String id) {
+        return userService.getUserProjects(id);
+    }
+
+    @PutMapping("/project/task/{userId}/{projectId}")
+    public Task updateTask(@PathVariable("userId") final String userId,
+                           @PathVariable("projectId") final String projectId,
+                           @RequestBody final Task task) {
+        return userService.updateTask(userId, projectId, task);
+    }
+
+    @GetMapping("/project/task/{userId}/{projectId}")
+    public List<Task> findAllTasks(@PathVariable("userId") final String userId,
+                                   @PathVariable("projectId") final String projectId) {
+        return userService.findAllTask(userId, projectId);
+    }
+
+    @DeleteMapping("/project/task/{userId}/{projectId}/{taskId}")
+    public Task deleteTask(@PathVariable("userId") final String userId,
+                           @PathVariable("projectId") final String projectId,
+                           @PathVariable("taskId") final String taskId) {
+        return userService.deleteTask(userId, projectId, taskId);
     }
 
 }
