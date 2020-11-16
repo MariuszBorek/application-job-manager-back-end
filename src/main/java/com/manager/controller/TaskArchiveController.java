407f152806c3ca8ff3ca4522763209450afe4271
Mariusz Borek <mariuszborek242@gmail.com>
2020-11-17 00:07:51 +0100

    added archivization of the tasks

---
+++ src/main/java/com/manager/controller/TaskArchiveController.java
@@ -0,0 +1,28 @@
+package com.manager.controller;
+
+import com.manager.model.Task;
+import com.manager.model.TaskArchive;
+import com.manager.service.TaskArchiveService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users/projects/tasks/archive")
+@CrossOrigin("*")
+public class TaskArchiveController {
+
+    private final TaskArchiveService taskArchiveService;
+
+    public TaskArchiveController(TaskArchiveService taskArchiveService) {
+        this.taskArchiveService = taskArchiveService;
+    }
+
+    @PostMapping("/{userId}/{projectId}")
+    public List<TaskArchive> clearFinishedTasks(@PathVariable("userId") final String userId,
+                                                @PathVariable("projectId") final String projectId,
+                                                @RequestBody final List<Task> tasks) {
+        return taskArchiveService.addArchivedTasks(userId, projectId, tasks);
+    }
+
+}
---
+++ src/main/java/com/manager/service/TaskArchiveService.java
@@ -0,0 +1,67 @@
+package com.manager.service;
+
+import com.manager.model.Project;
+import com.manager.model.Task;
+import com.manager.model.TaskArchive;
+import com.manager.repository.TaskArchiveRepository;
+import com.manager.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskArchiveService {
+
+    private final UserRepository userRepository;
+    private final TaskArchiveRepository taskArchiveRepository;
+
+    public TaskArchiveService(UserRepository userRepository, TaskArchiveRepository taskArchiveRepository) {
+        this.userRepository = userRepository;
+        this.taskArchiveRepository = taskArchiveRepository;
+    }
+
+    public List<TaskArchive> addArchivedTasks(String userId, String projectId, List<Task> tasks) {
+        List<TaskArchive> taskArchivesList = new ArrayList<>();
+        TaskArchive taskArchive = new TaskArchive();
+        for (Task task: tasks) {
+            if (task.getExecution().equals(true)) {
+                taskArchive.setId(task.getId());
+                taskArchive.setTopic(task.getTopic());
+                taskArchive.setText(task.getText());
+                taskArchive.setDate(task.getDate());
+                taskArchive.setPriority(task.getPriority());
+                taskArchive.setExecution(task.getExecution());
+                taskArchive.setProject(getUserProject(userId, projectId));
+                taskArchiveRepository.save(taskArchive);
+                taskArchivesList.add(taskArchive);
+            }
+
+        }
+        return getUserProject(userId, projectId).getTaskArchive();
+    }
+
+    private Project getUserProject(String userId, String projectId) {
+        return userRepository.findById(Integer.parseInt(userId))
+                .orElseThrow()
+                .getProjects()
+                .stream()
+                .filter(project -> project.getId().equals(Integer.parseInt(projectId)))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private TaskArchive getArchivedTask(String userId, String projectId, String taskArchivedId) {
+        return userRepository.findById(Integer.parseInt(userId))
+                .orElseThrow()
+                .getProjects().stream()
+                .filter(project -> project.getId().equals(Integer.parseInt(projectId)))
+                .findFirst()
+                .orElseThrow()
+                .getTaskArchive().stream()
+                .filter(tskArch -> tskArch.getId().equals(Integer.parseInt(taskArchivedId)))
+                .findFirst()
+                .orElseThrow();
+    }
+
+}
