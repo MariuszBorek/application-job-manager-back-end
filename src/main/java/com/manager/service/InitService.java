e62aa8c97a2c9936685379126b0dffaae115770b
Mariusz Borek <mariuszborek242@gmail.com>
2021-02-03 00:44:38 +0100

    turn of dev profile

--- src/main/java/com/manager/service/InitService.java
+++ src/main/java/com/manager/service/InitService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 
 @Service
-@Profile("dev")
+//@Profile("dev")
 public class InitService {
 
     private final UserService userService;
